3a6b86f871c45ca5146c25828a947570c146706d
YJ <18298405849@163.com>
2017-06-21 17:13:42 +0800

    recordShow页面（尝试用framework7框架来写，未完~）

---
+++ js/recordShow.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yangjiao on 2017/6/21.
+ */
+
+
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add views
+var view1 = myApp.addView('#view-1');
+var view2 = myApp.addView('#view-2', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+var view3 = myApp.addView('#view-3');
+
